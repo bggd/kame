813c527fd3d7584ee24159918d966fa2057d777b
bggd <gameovera@live.com>
2023-01-16 19:15:00 +0900

    update

--- src/lua/lua.cpp
+++ src/lua/lua.cpp
@@ -4,6 +4,26 @@
 #include "math/vector4.hpp"
 #include "math/matrix.hpp"
 
+// copy from lua.c
+/*
+** Message handler used to run all chunks
+*/
+static int msghandler(lua_State* L)
+{
+    const char* msg = lua_tostring(L, 1);
+    if (msg == NULL)
+    {                                            /* is error object not a string? */
+        if (luaL_callmeta(L, 1, "__tostring") && /* does it have a metamethod */
+            lua_type(L, -1) == LUA_TSTRING)      /* that produces a string? */
+            return 1;                            /* that is the message */
+        else
+            msg = lua_pushfstring(L, "(error object is a %s value)",
+                                  luaL_typename(L, 1));
+    }
+    luaL_traceback(L, L, msg, 1); /* append a standard traceback */
+    return 1;                     /* return the traceback */
+}
+
 namespace kame::lua {
 
 void Lua::initLua()
@@ -48,7 +68,7 @@ void Lua::pop(int n)
 
 int Lua::doString(const char* code)
 {
-    int status = luaL_dostring(L, code);
+    int status = luaL_loadstring(L, code);
     if (status != LUA_OK)
     {
         const char* statusStr = _statusCodeToString(status);
@@ -58,6 +78,19 @@ int Lua::doString(const char* code)
         SPDLOG_INFO("status: [{}], error msg: {}", statusStr, errMsg);
         pop();
     }
+    int base = lua_gettop(L);
+    lua_pushcfunction(L, msghandler);
+    lua_insert(L, base);
+    status = lua_pcall(L, 0, LUA_MULTRET, base);
+    if (status != LUA_OK)
+    {
+        const char* statusStr = _statusCodeToString(status);
+        size_t len = 0;
+        const char* pStr = lua_tolstring(L, -1, &len);
+        std::string errMsg(pStr, len);
+        SPDLOG_INFO("status: [{}], error msg: {}", statusStr, errMsg);
+    }
+    lua_remove(L, base);
     return status;
 }
 
