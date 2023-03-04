a8b5d7d8c9a6974b53b9e6b016aecd557f0c83a8
bggd <gameovera@live.com>
2023-03-04 19:37:54 +0900

    display uv

--- tool/uvview/main.cpp
+++ tool/uvview/main.cpp
@@ -31,6 +31,39 @@ struct Mesh {
     std::vector<unsigned int> indices;
 };
 
+struct VBOMesh {
+    kame::ogl::VertexBuffer* vboPositions = nullptr;
+    std::vector<kame::ogl::VertexBuffer*> vboUVSets;
+    kame::ogl::IndexBuffer* iboIndices = nullptr;
+
+    VBOMesh(const Mesh& mesh)
+    {
+        vboPositions = kame::ogl::createVertexBuffer(mesh.positions.size() * 3 * sizeof(float), GL_STATIC_DRAW);
+        vboPositions->setBuffer((const float*)&mesh.positions[0]);
+
+        vboUVSets.reserve(mesh.uvSets.size());
+        for (const auto& uv : mesh.uvSets)
+        {
+            auto* vboTexcoords = kame::ogl::createVertexBuffer(uv.size() * 2 * sizeof(float), GL_STATIC_DRAW);
+            vboTexcoords->setBuffer((const float*)&uv[0]);
+            vboUVSets.push_back(vboTexcoords);
+        }
+
+        iboIndices = kame::ogl::createIndexBuffer(mesh.indices.size() * sizeof(unsigned int), GL_STATIC_DRAW);
+        iboIndices->setBuffer((const unsigned int*)&mesh.indices[0]);
+    }
+
+    ~VBOMesh()
+    {
+        kame::ogl::deleteIndexBuffer(iboIndices);
+        for (auto& uv : vboUVSets)
+        {
+            kame::ogl::deleteVertexBuffer(uv);
+        }
+        kame::ogl::deleteVertexBuffer(vboPositions);
+    }
+};
+
 std::vector<kame::math::Vector3> toVertexPositions(const kame::gltf::Gltf* gltf, const kame::gltf::Mesh& m)
 {
     std::vector<kame::math::Vector3> positions;
@@ -139,6 +172,49 @@ std::vector<unsigned int> toVertexIndices(const kame::gltf::Gltf* gltf, const ka
     return indices;
 }
 
+const char* vertGLSL = R"(#version 330
+in vec2 vUV;
+uniform mat4 uMVP;
+void main() {
+    gl_Position = uMVP * vec4(vUV.x, vUV.y, 0.0, 1.0);
+}
+)";
+
+const char* fragGLSL = R"(#version 330
+out vec4 fragColor;
+void main() {
+    fragColor = vec4(0.278431, 0.447059, 0.701961, 0.25);
+}
+)";
+
+const char* flippedUVGLSL = R"(#version 330
+out vec4 fragColor;
+void main() {
+    if (gl_FrontFacing) {
+        discard;
+    }
+    else {
+        fragColor = vec4(0.701961, 0.278431, 0.278431, 0.25);
+    }
+}
+)";
+
+const char* drawLinesGLSL = R"(#version 330
+out vec4 fragColor;
+void main() {
+    fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+}
+)";
+
+const char* drawPointsGLSL = R"(#version 330 core
+out vec4 fragColor;
+void main() {
+    fragColor = vec4(1.0);
+}
+)";
+
+using namespace kame::math;
+
 int main(int argc, char** argv)
 {
     if (argc < 2)
@@ -151,9 +227,14 @@ int main(int argc, char** argv)
 
     kame::sdl::WindowOGL win;
     win.setOglDebugMode(true);
-    win.forceGLVersion(3, 3, true);
-    win.openWindow();
+    win.forceGLVersion(3, 3, false);
+    win.openWindow("uvview", 512, 512);
     win.setVsync(true);
+
+    kame::ogl::Shader* shaderFrontFace = kame::ogl::createShader(vertGLSL, fragGLSL);
+    kame::ogl::Shader* shaderBackFace = kame::ogl::createShader(vertGLSL, flippedUVGLSL);
+    kame::ogl::Shader* shaderDrawLines = kame::ogl::createShader(vertGLSL, drawLinesGLSL);
+    kame::ogl::Shader* shaderDrawPoints = kame::ogl::createShader(vertGLSL, drawPointsGLSL);
 
     kame::gltf::Gltf* gltf = kame::gltf::loadGLTF(argv[1]);
     int numUVSet = getNumberOfUVSet(gltf);
@@ -164,6 +245,9 @@ int main(int argc, char** argv)
         return 1;
     }
     std::vector<Mesh> meshes;
+    std::vector<VBOMesh> vboMeshes;
+    meshes.reserve(gltf->meshes.size());
+    vboMeshes.reserve(gltf->meshes.size());
     for (auto& m : gltf->meshes)
     {
         meshes.emplace_back();
@@ -172,6 +256,19 @@ int main(int argc, char** argv)
         mesh.positions = toVertexPositions(gltf, m);
         mesh.uvSets = toVertexUVSets(gltf, m);
         mesh.indices = toVertexIndices(gltf, m);
+
+        vboMeshes.emplace_back(mesh);
+    }
+    kame::gltf::deleteGLTF(gltf);
+
+    std::vector<kame::ogl::VertexArrayObject> vaoList;
+    for (auto& vbo : vboMeshes)
+    {
+        auto vao = kame::ogl::VertexArrayObjectBuilder()
+                       .bindAttribute(shaderFrontFace->getAttribLocation("vUV"), vbo.vboUVSets[0], 2, 2 * sizeof(float), 0)
+                       .bindIndexBuffer(vbo.iboIndices)
+                       .build();
+        vaoList.push_back(vao);
     }
 
     for (;;)
@@ -180,10 +277,67 @@ int main(int argc, char** argv)
         auto state = win.getState();
         if (state.isCloseRequest)
             break;
+
+        kame::ogl::setViewport(0, 0, state.drawableSizeX, state.drawableSizeY);
+        kame::ogl::setClearBuffer(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT, Vector4(0.5, 0.5, 0.5, 1));
+        kame::ogl::RasterizerState rasterState = kame::ogl::RasterizerStateBuilder().build();
+        kame::ogl::setRasterizerState(rasterState);
+        kame::ogl::BlendState blendState = kame::ogl::BlendStateBuilder()
+                                               .blendEquation(GL_FUNC_ADD, GL_FUNC_ADD)
+                                               .blendFunction(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
+                                               .build();
+        kame::ogl::setBlendState(blendState);
+
+        auto view = Matrix::createLookAt(Vector3(0.0f, 0.0f, 0.5f), Vector3(0.0f, 0.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f));
+        auto proj = Matrix::createOrthographic_NO(0.0f, 1.0f, 1.0f, 0.0f, 0.1f, 10.0f);
+
+        kame::ogl::setShader(shaderFrontFace);
+        shaderFrontFace->setMatrix("uMVP", view * proj);
+        int idx = 0;
+        for (auto& m : meshes)
+        {
+            vaoList[idx].drawElements(GL_TRIANGLES, m.indices.size(), GL_UNSIGNED_INT);
+            ++idx;
+        }
+
+        kame::ogl::setShader(shaderBackFace);
+        shaderBackFace->setMatrix("uMVP", view * proj);
+        idx = 0;
+        for (auto& m : meshes)
+        {
+            vaoList[idx].drawElements(GL_TRIANGLES, m.indices.size(), GL_UNSIGNED_INT);
+            ++idx;
+        }
+
+        kame::ogl::setShader(shaderDrawLines);
+        shaderDrawLines->setMatrix("uMVP", view * proj);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        idx = 0;
+        for (auto& m : meshes)
+        {
+            vaoList[idx].drawElements(GL_TRIANGLES, m.indices.size(), GL_UNSIGNED_INT);
+            ++idx;
+        }
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+        kame::ogl::setShader(shaderDrawPoints);
+        shaderDrawPoints->setMatrix("uMVP", view * proj);
+        idx = 0;
+        for (auto& m : meshes)
+        {
+            vaoList[idx].drawElements(GL_POINTS, m.indices.size(), GL_UNSIGNED_INT);
+            ++idx;
+        }
+
         win.swapWindow();
     }
 
-    kame::gltf::deleteGLTF(gltf);
+    vaoList.clear();
+    vboMeshes.clear();
+    kame::ogl::deleteShader(shaderDrawPoints);
+    kame::ogl::deleteShader(shaderDrawLines);
+    kame::ogl::deleteShader(shaderBackFace);
+    kame::ogl::deleteShader(shaderFrontFace);
     win.closeWindow();
     kame::kameShutdown();
     return 0;
