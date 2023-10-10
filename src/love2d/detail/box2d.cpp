0574a88cac11ad176fe10298ee957c5641baeda9
bggd <gameovera@live.com>
2023-10-10 12:19:22 +0900

    namespace

--- src/love2d/detail/box2d.cpp
+++ src/love2d/detail/box2d.cpp
@@ -2,7 +2,9 @@
 
 std::unordered_map<b2Fixture*, kame::love2d::detail::box2d::SPtrBox2dFixture::weak_type> kame::love2d::detail::box2d::fixtureMap;
 
-const std::vector<float>& kame::love2d::detail::box2d::Box2dPolygonShape::getPoints()
+namespace kame::love2d::detail::box2d {
+
+const std::vector<float>& Box2dPolygonShape::getPoints()
 {
     auto& ctx = kame::love2d::detail::Context::getInstance();
     assert(ctx.isValid());
@@ -17,7 +19,7 @@ const std::vector<float>& kame::love2d::detail::box2d::Box2dPolygonShape::getPoi
     return _points;
 }
 
-kame::love2d::detail::box2d::Box2dWorld::~Box2dWorld()
+Box2dWorld::~Box2dWorld()
 {
     assert(_worldB2D);
     assert(!_worldB2D->IsLocked());
@@ -32,7 +34,7 @@ kame::love2d::detail::box2d::Box2dWorld::~Box2dWorld()
     _worldB2D = nullptr;
 }
 
-kame::love2d::detail::box2d::Box2dBody::~Box2dBody()
+Box2dBody::~Box2dBody()
 {
     assert(_bodyB2D);
     assert(!_parentWorld->_worldB2D->IsLocked());
@@ -41,7 +43,7 @@ kame::love2d::detail::box2d::Box2dBody::~Box2dBody()
     _bodyB2D = nullptr;
 }
 
-kame::love2d::detail::box2d::Box2dFixture::~Box2dFixture()
+Box2dFixture::~Box2dFixture()
 {
     assert(_fixtureB2D);
     assert(!_parentBody->_bodyB2D->GetWorld()->IsLocked());
@@ -58,21 +60,21 @@ kame::love2d::detail::box2d::Box2dFixture::~Box2dFixture()
     fixtureMap.erase(fixtureMap.find(_fixtureB2D));
     _fixtureB2D = nullptr;
 }
-float kame::love2d::detail::box2d::Box2dBody::getX()
+float Box2dBody::getX()
 {
     auto& ctx = kame::love2d::detail::Context::getInstance();
     assert(ctx.isValid());
     return ctx.physics->scaleUp(_bodyB2D->GetPosition().x);
 }
 
-float kame::love2d::detail::box2d::Box2dBody::getY()
+float Box2dBody::getY()
 {
     auto& ctx = kame::love2d::detail::Context::getInstance();
     assert(ctx.isValid());
     return ctx.physics->scaleUp(_bodyB2D->GetPosition().y);
 }
 
-std::vector<float> kame::love2d::detail::box2d::Box2dBody::getWorldPoints(std::vector<float>& points)
+std::vector<float> Box2dBody::getWorldPoints(std::vector<float>& points)
 {
     auto& ctx = kame::love2d::detail::Context::getInstance();
     assert(ctx.isValid());
@@ -101,9 +103,9 @@ static Box2dDestructionListener destructionListener;
 
 static kame::love2d::detail::box2d::DebugDraw debugDraw;
 
-kame::love2d::detail::box2d::SPtrBox2dWorld kame::love2d::detail::box2d::newSPtrBox2dWorld(b2Vec2 gravity, bool sleep)
+SPtrBox2dWorld newSPtrBox2dWorld(b2Vec2 gravity, bool sleep)
 {
-    auto* w = new kame::love2d::detail::box2d::Box2dWorld();
+    auto* w = new Box2dWorld();
     assert(w);
 
     w->_worldB2D = new b2World(gravity);
@@ -115,12 +117,12 @@ kame::love2d::detail::box2d::SPtrBox2dWorld kame::love2d::detail::box2d::newSPtr
     debugDraw.SetFlags(b2Draw::e_shapeBit);
     w->_worldB2D->SetDebugDraw(&debugDraw);
 
-    return kame::love2d::detail::box2d::SPtrBox2dWorld(w);
+    return SPtrBox2dWorld(w);
 }
 
-kame::love2d::detail::box2d::SPtrBox2dBody kame::love2d::detail::box2d::newSPtrBox2dBody(kame::love2d::detail::box2d::SPtrBox2dWorld w, float x, float y, b2BodyType type)
+SPtrBox2dBody newSPtrBox2dBody(SPtrBox2dWorld w, float x, float y, b2BodyType type)
 {
-    auto* b = new kame::love2d::detail::box2d::Box2dBody();
+    auto* b = new Box2dBody();
     assert(b);
 
     b2BodyDef def = b2BodyDef();
@@ -131,25 +133,25 @@ kame::love2d::detail::box2d::SPtrBox2dBody kame::love2d::detail::box2d::newSPtrB
 
     b->_parentWorld = w;
 
-    return kame::love2d::detail::box2d::SPtrBox2dBody(b);
+    return SPtrBox2dBody(b);
 }
 
-kame::love2d::detail::box2d::SPtrBox2dFixture kame::love2d::detail::box2d::newSPtrBox2DFixture(kame::love2d::detail::box2d::SPtrBox2dBody body, const kame::love2d::detail::box2d::Box2dShape& shape, float density)
+SPtrBox2dFixture newSPtrBox2DFixture(SPtrBox2dBody body, const Box2dShape& shape, float density)
 {
-    auto* f = new kame::love2d::detail::box2d::Box2dFixture();
+    auto* f = new Box2dFixture();
     assert(f);
 
     b2FixtureDef def = b2FixtureDef();
     def.density = density;
 
-    if (std::holds_alternative<kame::love2d::detail::box2d::Box2dCircleShape>(shape))
+    if (std::holds_alternative<Box2dCircleShape>(shape))
     {
-        auto& s = std::get<kame::love2d::detail::box2d::Box2dCircleShape>(shape);
+        auto& s = std::get<Box2dCircleShape>(shape);
         def.shape = &s._circleShapeB2D;
     }
     else
     {
-        auto& s = std::get<kame::love2d::detail::box2d::Box2dPolygonShape>(shape);
+        auto& s = std::get<Box2dPolygonShape>(shape);
         def.shape = &s._polygonShapeB2D;
     }
 
@@ -158,19 +160,21 @@ kame::love2d::detail::box2d::SPtrBox2dFixture kame::love2d::detail::box2d::newSP
 
     f->_parentBody = body;
 
-    auto sptr = kame::love2d::detail::box2d::SPtrBox2dFixture(f);
+    auto sptr = SPtrBox2dFixture(f);
 
     fixtureMap[f->_fixtureB2D] = sptr;
 
     return sptr;
 }
 
-void kame::love2d::detail::box2d::Box2dWorld::update(float dt, int velocityiterations, int positioniterations)
+void Box2dWorld::update(float dt, int velocityiterations, int positioniterations)
 {
     _worldB2D->Step(dt, velocityiterations, positioniterations);
 }
 
-void kame::love2d::detail::box2d::Box2dWorld::debugDraw()
+void Box2dWorld::debugDraw()
 {
     _worldB2D->DebugDraw();
 }
+
+} // namespace kame::love2d::detail::box2d
