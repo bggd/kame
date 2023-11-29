608a4b1e73003df917ddc094e9fe955553ab8778
bggd <gameovera@live.com>
2023-11-29 11:31:32 +0900

    update sdl

--- src/all.hpp
+++ src/all.hpp
@@ -7,10 +7,17 @@
 #pragma GCC diagnostic ignored "-Wstringop-overflow="
 #pragma GCC diagnostic ignored "-Wimplicit-fallthrough="
 #endif
+#if __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
 #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
 #include <spdlog/spdlog.h>
 #if __GCC__
 #pragma GCC disgnostic pop
+#endif
+#if __clang__
+#pragma clang diagnostic pop
 #endif
 
 #include <stb_image.h>
