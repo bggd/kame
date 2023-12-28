c32f0e52d691013e14b246f4a6c2e14d8017a108
bggd <gameovera@live.com>
2023-12-28 23:11:08 +0900

    comment'

--- src/kame.cpp
+++ src/kame.cpp
@@ -13,6 +13,7 @@ bool isInit = false;
 
 void kameInit()
 {
+    // TODO: KAME_SPDLOG_LEVEL_DEBUG
     spdlog::set_level(spdlog::level::debug);
     spdlog::info("kameInit");
     // stbi_set_flip_vertically_on_load(true);
